428506c132ac8e8ae8ac28a669dd546a98f65be0
Tasneem Wadee <tasneemw026@gmail.com>
2023-02-14 09:16:51 +0000

    Update 3 files

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,18 +1,30 @@
 
+//import { FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
-function FeedbackItem({item}) {
+function FeedbackItem({item, handleDelete}) {
+ 
+ 
+ /*const handleClick = (id) => {
+   console.log(id)
+   alert('Clicked on x_id delete')
+ }
+ */
   return (
+    <>
     <Card >
       <div className="num-display"> {item.rating}</div>
+    <button onClick={() => handleDelete(item.id)} className='close'> x </button> 
       <div className="text-display"> {item.text}</div>
+      
     </Card>
+    
+    </>
   );
 }
 
 FeedbackItem.propTypes ={
-    item: PropTypes.object.isRequired,
-   
+    item: PropTypes.object.isRequired,  
 }
 
 export default FeedbackItem;
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,7 +2,9 @@
 import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 import React from 'react'
-function FeedbackList({feedback}){
+
+
+function FeedbackList({feedback, handleDelete}){
    if(!feedback || feedback.length ===0){
        return <p>nothing yet</p>
    }
@@ -10,7 +12,9 @@ function FeedbackList({feedback}){
     return(
         <div className='feedback-list'>
             {feedback.map((item) => (
-                <FeedbackItem key={item.id} item={item} />
+                <FeedbackItem key={item.id}
+                item={item} 
+                handleDelete={handleDelete}/>
             ))}
             <p className='p-para'> are you showing </p>
         </div>
